f2bf56cba4536840e7932e778bae52846ce32ef7
Aaron Feledy <aaron@arrow.one>
2026-05-31 16:27:25 -0500

    Sort scratch recipe test imports

--- core/test/services/scratch-recipe.test.ts
+++ core/test/services/scratch-recipe.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import { mkdtemp, readdir, readFile, realpath, rm, stat } from "node:fs/promises";
+import { mkdtemp, readFile, readdir, realpath, rm, stat } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import { Effect, Layer, Stream } from "effect";
