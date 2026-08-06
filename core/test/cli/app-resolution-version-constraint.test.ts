bc1181e9f082a4eb456cc8969c84d291f2cf0066
Aaron Feledy <aaron@arrow.one>
2026-08-06 14:14:53 -0500

    Use host include adapter in version constraint test

--- core/test/cli/app-resolution-version-constraint.test.ts
+++ core/test/cli/app-resolution-version-constraint.test.ts
@@ -9,11 +9,10 @@ import { type Context, Effect, Layer, Schema } from "effect";
 import { LandofileShape } from "@lando/sdk/schema";
 import { LandofileService, Renderer } from "@lando/sdk/services";
 
-import { resolveLandofileIncludes } from "@lando/landofile/includes";
 import { assertLandoVersionConstraint, loadUserLandofile } from "../../src/cli/app-resolution.ts";
 import { runWithRendererHandling } from "../../src/cli/renderer-boundary.ts";
 import { createBufferedRendererIO } from "../../src/cli/renderer/io.ts";
-import { LandofileServiceLive } from "../../src/services/landofile-live.ts";
+import { LandofileServiceLive, resolveLandofileIncludes } from "../../src/services/landofile-live.ts";
 
 const landofile = (lando?: string): LandofileShape => (lando === undefined ? {} : { lando });
 
