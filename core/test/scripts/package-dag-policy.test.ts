34503f452a5bb79976662a13e9afdbb1e77bcfdb
Aaron Feledy <aaron@arrow.one>
2026-08-10 18:59:18 -0500

    test: lock managed-file package direction

--- core/test/scripts/package-dag-policy.test.ts
+++ core/test/scripts/package-dag-policy.test.ts
@@ -3,6 +3,20 @@ import { describe, expect, test } from "bun:test";
 import { WORKSPACE_EDGE_TABLE } from "../../../scripts/boundary/rules/package-dag-policy.ts";
 
 describe("workspace package DAG policy", () => {
+  test("allows the managed-file package to depend only on its primitive inputs", () => {
+    // Given / When
+    const managedFilePolicy = WORKSPACE_EDGE_TABLE["@lando/managed-file"];
+    const enginePolicy = WORKSPACE_EDGE_TABLE["@lando/engine"];
+
+    // Then
+    expect(managedFilePolicy).toEqual({
+      dependencies: ["@lando/sdk", "@lando/paths", "@lando/state-store", "@lando/redaction"],
+      devDependencies: [],
+    });
+    expect(enginePolicy?.dependencies).not.toContain("@lando/managed-file");
+    expect(enginePolicy?.devDependencies).not.toContain("@lando/managed-file");
+  });
+
   test("allows only core to depend on engine", () => {
     // Given
     const policies = Object.entries(WORKSPACE_EDGE_TABLE);
